1c894aedabddd489b87b584c1755e00ac414d9e2
Tyler Higgs <thiggs911@gmail.com>
2025-10-07 11:04:20 -0600

    fixes tests

--- frontend/src/app/pages/collaborate/collaborate.component.spec.ts
+++ frontend/src/app/pages/collaborate/collaborate.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { CollaborateComponent } from './collaborate.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('CollaborateComponent', () => {
+  let component: CollaborateComponent;
+  let fixture: ComponentFixture<CollaborateComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [CollaborateComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(CollaborateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ frontend/src/app/pages/collaborate/collaborate.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborate',
+  imports: [],
+  templateUrl: './collaborate.component.html',
+  styleUrl: './collaborate.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CollaborateComponent {
+
+}
